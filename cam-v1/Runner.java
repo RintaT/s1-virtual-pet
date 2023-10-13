4faebe62a279111dff09e9fc9b0cf257ed3cea7c
Your Name <you@example.com>
2023-10-13 14:14:50 -0700

    w9d1

---
+++ cam-v1/Runner.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+
+public class Runner {
+    
+    public Runner(){
+
+        VirtualPet v = new VirtualPet();
+        
+        
+    }
+
+    public static void main(String[] args) {
+        new Runner();
+        v.sleep();
+    }
+
+    String s = (String)JOptionPane.showInputDialog(
+                    new JFrame(),
+                    "Your Message",
+                    "Your Title",
+                    JOptionPane.PLAIN_MESSAGE
+);
+
+    String s2 = (String)JOptionPane.showInputDialog(
+    new JFrame(),
+    "How many laps should I run?",
+    "Question?",
+    JOptionPane.PLAIN_MESSAGE
+  );
+int x = Integer.parseInt(s);
+
+
+
+
+
+}
