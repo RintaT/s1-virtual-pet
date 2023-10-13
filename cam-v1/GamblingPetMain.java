034b7c92498b01d616e25ce7259fa23296453f58
Your Name <you@example.com>
2023-10-13 15:05:03 -0700

    w9d2q1

---
+++ cam-v1/GamblingPetMain.java
@@ -0,0 +1,12 @@
+public class GamblingPetMain {
+    public static void main(String[] args) {
+        GamblingPet g1 = new GamblingPet(10);
+
+        System.out.println(g1.DiceGame(5000));
+
+
+
+
+
+    }
+}
